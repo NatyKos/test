bf509d59df59de4405c12973a9731791d12c251a
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-01-14 12:25:19 +0000

    Update test5.js

--- js/test5.js
+++ js/test5.js
@@ -168,33 +168,33 @@ const oddNumbers = numbers.filter(number => number % 2 !== 0);
 console.log(evenNumbers);
 console.log(oddNumbers);
 // =============================
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "The Dream of a Ridiculous Man",
-    author: "Fyodor Dostoevsky",
-    rating: 7.75,
-  },
-  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
-  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
-];
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
 
-const MIN_RATING = 8;
-const AUTHOR = "Bernard Cornwell";
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
 
-const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
-const booksByAuthor = books.filter(book => book.author === AUTHOR);
-console.log(topRatedBooks);
-console.log(booksByAuthor);
+// const topRatedBooks = books.filter(book => book.rating >= MIN_RATING);
+// const booksByAuthor = books.filter(book => book.author === AUTHOR);
+// console.log(topRatedBooks);
+// console.log(booksByAuthor);
 // ====================================
 // const getUsersWithEyeColor = (users, color) => {return users.filter(user => user.eyeColor === color) };
 // console.log(getUsersWithEyeColor([
@@ -266,7 +266,11 @@ console.log(booksByAuthor);
 const getUsersWithAge = (users, minAge, maxAge) => 
      users.filter(user => (minAge <= user.age && user.age <= maxAge));
 ;
-console.log(getUsersWithAge([
+const getUserWithEmail = (users, email) => 
+    users.find((user) => user.email === email)
+;
+
+console.log(getUserWithEmail([
   {
     name: "Moore Hensley",
     email: "moorehensley@indexia.com",
@@ -337,4 +341,132 @@ console.log(getUsersWithAge([
     gender: "female",
     age: 39
   }
-], 20, 30));
+], "shereeanthony@kog.com"));
+// =========================
+// const players = {
+//   mango: 1270,
+//   poly: 468,
+//   ajax: 710,
+//   kiwi: 244,
+// };
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
+
+// const totalPlayTime = playtimes.reduce((t, number) => t+number );
+
+// const averagePlayTime = totalPlayTime / playtimes.length;
+// console.log(totalPlayTime);
+// ======================
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+
+const totalAveragePlaytimePerGame = players.reduce(
+    (timeGame, player) =>
+     player.playtime / player.gamesPlayed + timeGame, 0);
+console.log(totalAveragePlaytimePerGame);
+// ======================
+// const calculateTotalBalance = (users) => {
+//     return users.reduce(
+//         (total, user) =>
+//             user.balance + total, 0)
+// }
+// ;
+// console.log(calculateTotalBalance([
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female"
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male"
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female"
+//   }
+// ]));
+// ==============================
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  {
+    title: "The Dreams in the Witch House",
+    author: "Howard Lovecraft",
+    rating: 8.67,
+  },
+];
+const MIN_BOOK_RATING = 8;
+
+const names = books
+    .filter(book => book.rating > MIN_BOOK_RATING)
+    .map(book => book.author)
+    .toSorted((a, b) => a.localeCompare(b));
+console.log(names);
